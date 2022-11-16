25f277f4f795a0fa00940590ff75913b54f1c453
Antonio Curavalea <112291302+arqalite@users.noreply.github.com>
2022-11-17 01:03:59 +0200

    fix: adjust viewport on every screen

--- src/frontend/mod.rs
+++ src/frontend/mod.rs
@@ -8,9 +8,12 @@ mod settings;
 
 use crate::prelude::*;
 use dioxus::prelude::*;
+use dioxus::web::use_eval;
 
 pub fn render_screen(cx: Scope) -> Element {
     let state = use_atom_ref(&cx, STATE);
+
+    use_eval(&cx)("var viewport=document.querySelector(\"meta[name=viewport]\");viewport.setAttribute(\"content\",viewport.content+\", height=\"+window.innerHeight),viewport.setAttribute(\"content\",viewport.content+\", width=\"+window.innerWidth);");
 
     cx.render(rsx!(
         div {
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ pub fn main() {
 
     dioxus::web::launch(|cx| {
         let state = use_atom_ref(&cx, STATE);
-
+        
         if !state.read().checked_storage {
             state.write().load_existing_game();
             state.write().settings.load();

ec0e5de53d094b54dc9daeee10191d7f2b68d383
Antonio Curavalea <112291302+arqalite@users.noreply.github.com>
2022-11-18 13:59:13 +0200

    fix: removed broken workbox code from SW

--- assets/js/sw.js
+++ assets/js/sw.js
@@ -2,21 +2,6 @@
 
 const CACHE = "rummy_nights-0.3";
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
-
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
-});
-
-workbox.routing.registerRoute(
-  new RegExp('/*'),
-  new workbox.strategies.StaleWhileRevalidate({
-    cacheName: CACHE
-  })
-);
-
 // Add whichever assets you want to precache here:
 const PRECACHE_ASSETS = [
   '/img/',
@@ -27,17 +12,17 @@ const PRECACHE_ASSETS = [
 // Listener for the install event - precaches our assets list on service worker install.
 self.addEventListener('install', event => {
   event.waitUntil((async () => {
-      const cache = await caches.open(CACHE_NAME);
-      cache.addAll(PRECACHE_ASSETS);
+    const cache = await caches.open(CACHE_NAME);
+    cache.addAll(PRECACHE_ASSETS);
   })());
 });
 
 self.addEventListener('activate', event => {
-event.waitUntil(clients.claim());
+  event.waitUntil(clients.claim());
 });
 
 self.addEventListener('fetch', event => {
-event.respondWith(async () => {
+  event.respondWith(async () => {
     const cache = await caches.open(CACHE_NAME);
 
     // match the request to our cache
@@ -45,11 +30,11 @@ event.respondWith(async () => {
 
     // check if we got a valid response
     if (cachedResponse !== undefined) {
-        // Cache hit, return the resource
-        return cachedResponse;
+      // Cache hit, return the resource
+      return cachedResponse;
     } else {
       // Otherwise, go to the network
-        return fetch(event.request)
+      return fetch(event.request)
     };
-});
+  });
 });

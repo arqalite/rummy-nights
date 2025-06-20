a3b391df18e321b340b85104eb485712ba1b14d9
Antonio Curavalea <one.kyonblack@gmail.com>
2025-06-20 23:07:13 +0300

    build: update Vercel build script

--- .vercel/build.sh
+++ .vercel/build.sh
@@ -4,4 +4,4 @@ curl https://sh.rustup.rs -sSf | sh -s -- -y
 source "$HOME/.cargo/env"
 rustup target add wasm32-unknown-unknown
 cargo install dioxus-cli
-/vercel/.cargo/bin/dx build --release
+/vercel/.cargo/bin/dx bundle -r --platform web
